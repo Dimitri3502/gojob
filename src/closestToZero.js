fa25c54f2df1fc36f3178317bc8357f7781626fe
TK6051 <dimitri.beubry@engie.com>
2020-09-24 17:40:38 +0200

    add some documentation

--- src/closestToZero.js
+++ src/closestToZero.js
@@ -1,3 +1,11 @@
+/**
+ *  function “closestToZero” described by the following specifications:
+ *      - given an array of positive and negative integers, it returns the closest number to zero
+ *      - if the closest number in input could be either negative or positive, the function returns the positive one
+ *      - if the input array is undefined or empty, the function returns 0
+ * @param {Array<number>} integerArray
+ * @returns {number}
+ */
 function closestToZero(integerArray) {
     let result = 0;
 
